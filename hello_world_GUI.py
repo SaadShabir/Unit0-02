fe0e50c25175fd33bea750a2172bdcd37a049d8f
Saad <isaadshabir@gmail.com>
2017-09-14 23:24:54 -0400

    Init

---
+++ hello_world_GUI.py
@@ -0,0 +1,10 @@
+# Created by: Saad Shabir
+# Created on: Aug 2017
+# Created for: IC3U
+# Daily Assignment - Unit 0-02
+# This program displays Hello World but in GUI
+
+import ui
+
+v = ui.load_view()
+v.present('sheet')
